d1205440e2a89a3f8fb7654c3e33c1bfe54a7866
Rasoul Unlimited <rasoul.unlimited@gmail.com>
2025-05-27 16:54:40 +0330

    Update

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 // Automatically update the current year in the footer
 document.getElementById('current-year').textContent = new Date().getFullYear();
+
+<script>
+AOS.init({
+  // Global settings:
+  disable: false,
+  startEvent: "DOMContentLoaded",
+  initClassName: "aos-init",
+  animatedClassName: "aos-animate",
+  useClassNames: false,
+  disableMutationObserver: false,
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120,
+  delay: 0,
+  duration: 800,
+  easing: "ease",
+  once: false,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
+</script>
