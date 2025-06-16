afd98ddd0730c309f80d93183bbdf992af0670db
Rasoul Unlimited <rasoul.unlimited@gmail.com>
2025-06-16 13:55:02 +0330

    Update

--- assets/js/performance-helpers.js
+++ assets/js/performance-helpers.js
@@ -1,6 +1,13 @@
-document.querySelectorAll('link[rel="preload"][as="style"][data-make-stylesheet]').forEach(link => {
-    link.addEventListener('load', function () {
-      this.rel = 'stylesheet';
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelectorAll('link[rel="preload"][as="style"][data-make-stylesheet]')
+    .forEach((preloadLink) => {
+      const realHref = preloadLink.getAttribute("data-href");
+      const newLink = document.createElement("link");
+      newLink.rel = "stylesheet";
+      newLink.href = realHref;
+      newLink.crossOrigin = preloadLink.crossOrigin || "anonymous";
+      newLink.referrerPolicy = preloadLink.referrerPolicy || "no-referrer";
+      preloadLink.parentNode.insertBefore(newLink, preloadLink.nextSibling);
     });
-  });
-  
+});
