632e406845744653e7fed577d3f78eae15b246e3
Rasoul Unlimited <rasoul.unlimited@gmail.com>
2025-06-29 01:18:02 +0330

    Update

--- assets/js/lang/en.js
+++ assets/js/lang/en.js
@@ -2,3 +2,4 @@ window.langStrings = {
     themeChanged: theme => `Theme changed to ${theme === 'dark' ? 'dark' : 'light'} mode.`,
     endOfPage: "You've reached the end of the page. Thank you for visiting! 🎉"
   };
+  
